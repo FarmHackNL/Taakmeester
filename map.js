06b21e894eedb56382d01faab73aa8649ed2ba83
Rony <rbnedkov@gmail.com>
2016-06-25 09:55:34 +0200

    Removing library error

--- map.js
+++ map.js
@@ -12,6 +12,15 @@ var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
+var OpenTopoMap = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+var grid_style = {
+
+}
+
 var obj = $.get("data/18m_grid.geojson")
 	.done(function(data) {
 		console.log(data);
