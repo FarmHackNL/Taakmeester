579bd02db00e0a797a7c3bab23a5ab4bffad8b95
Rony <rbnedkov@gmail.com>
2016-06-25 12:23:03 +0200

    Updated style of polygons based on mean value

--- map.js
+++ map.js
@@ -1,41 +1,98 @@
-var mymap = L.map('map').setView([53.0159, 7.018], 15);
+	var mymap = L.map('map').setView([53.0190, 7.018], 15);
 
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'your.mapbox.project.id',
-//     accessToken: 'your.mapbox.public.access.token'
-// }).addTo(mymap);
+	var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 12,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	});
 
-var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
+	// var OpenTopoMap = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	// 	maxZoom: 17,
+	// 	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+	// });
 
-var OpenTopoMap = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-});
+	var obj = $.get("data/18m_grid.geojson")
+		.done(function(data) {
+			console.log(data);
 
-var grid_style = {
+			var layer = L.geoJson($.parseJSON(data), {
+				onEachFeature: (function(feature, layer) {
+					if (feature.properties.mean < 0.1) {
+						console.log(feature.properties.mean)
+						layer.setStyle({
+						fillColor: '#a0d3aa',
+						fillOpacity: '0.9',
+						opacity: '0'
 
-}
+					});
 
-var obj = $.get("data/18m_grid.geojson")
-	.done(function(data) {
-		console.log(data);
+					}
 
-		var layer = L.geoJson($.parseJSON(data), {
-			onEachFeature: (function(feature, layer) {
-				layer.on('click', function() {
-					console.log(feature.properties.mean);
-				});
-			})
-		});
-		layer.addTo(mymap);
-		//console.log(tileLayer)
-	})
-	
+					if (feature.properties.mean < 0.2 && feature.properties.mean > 0.1) {
+						console.log(feature.properties.mean)
+						layer.setStyle({
+						fillColor: '#71bd7f',
+						fillOpacity: '0.9',
+						opacity: '0'
+
+					});
+
+					}
+
+					if (feature.properties.mean < 0.3 && feature.properties.mean > 0.2) {
+						console.log(feature.properties.mean)
+						layer.setStyle({
+						fillColor: '#42a755',
+						fillOpacity: '0.9',
+						opacity: '0'
+
+					});
+
+					}
+
+					if (feature.properties.mean < 0.4 && feature.properties.mean > 0.3) {
+						console.log(feature.properties.mean)
+						layer.setStyle({
+						fillColor: '#13912b',
+						fillOpacity: '0.9',
+						opacity: '0'
+
+					});
+
+					}
+
+					if (feature.properties.mean < 0.6 & feature.properties.mean > 0.4) {
+						console.log(feature.properties.mean)
+						layer.setStyle({
+						Color: '#008000',
+						fillColor: '#0d651e',
+						fillOpacity: '0.9',
+						opacity: '0'
+
+					});
+
+					}
+
+					if (feature.properties.mean < 1 & feature.properties.mean > 0.6) {
+						console.log(feature.properties.mean)
+						layer.setStyle({
+						Color: '#008000',
+						fillColor: '#052b0c',
+						fillOpacity: '0.9',
+						opacity: '0'
+
+					});
+
+					}
+
+					layer.on('click', function() {
+						console.log(feature.properties.mean);
+					});
+				})
+			});
+			layer.addTo(mymap);
+			//console.log(tileLayer)
+		})
+		
 
 
-OpenStreetMap_Mapnik.addTo(mymap);
+	OpenStreetMap_Mapnik.addTo(mymap);
